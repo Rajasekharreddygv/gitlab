de534c5f9e532e8bffac6b858652246a9954fc17
neha <neelu@gmail.com>
2018-08-27 13:19:55 +0000

    Cloned and Modified

--- welcome.java
+++ welcome.java
@@ -2,5 +2,7 @@ class Welcome{
 public static void main(String[] args){
 System.out.println("Welcome to DevOps Certification Program");
 System.out.println("### Initial Code Modified ###");
+System.out.println("### This code is entered at GitLab ###");
+System.out.println("### This code is modified after clone ###");
 }
 }
